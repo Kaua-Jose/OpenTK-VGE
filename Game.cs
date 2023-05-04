62e4cdb768d0d23653a8dbaa53db26c7f6638b38
Kaua-Jose <kaua.jose11222@outlook.com.br>
2023-05-04 17:35:34 -0300

    Esc key down

--- Game.cs
+++ Game.cs
@@ -2,6 +2,8 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace OpenTK_VGE.GameBackEnd
 {
@@ -13,8 +15,18 @@ namespace OpenTK_VGE.GameBackEnd
         }
         protected override void OnLoad()
         {
+
             GL.ClearColor(1.0f, 0.3f, 0.4f, 1.0f);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
             SwapBuffers();
         }
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            if(e.Key == Keys.Escape)Console.WriteLine("Esc Apertado");
+        }
+
     }
 }
